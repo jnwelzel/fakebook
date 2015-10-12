63f80510c139797465c19e5fe673a5012477bb0c
Jonathan Welzel <jnwelzel@gmail.com>
2015-10-12 17:12:57 -0300

    template do modal no cache do angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -34,11 +34,11 @@ angular.module('fakebookApp')
 
     $scope.openStatusModal = function () {
       ngDialog.open({
-        template: 'views/modals/status.html',
+        template: 'status-modal.html',
         className: 'ngdialog-theme-plain',
         scope: $scope,
-        cache: false
+        cache: true
       });
-    };
+    }
 
   }]);
